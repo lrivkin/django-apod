cd3799499778151cd00c4219cf670f0ce07e9fc6
Leah Rivkin <30099399+lrivkin@users.noreply.github.com>
2024-06-23 13:01:06 -0600

    scaffolding of the images view, url, and serializer

--- images/views.py
+++ images/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import APODImage
+from .serializers import APODImageSerializer
+
+class APODImageAPIView(generics.RetrieveAPIView):
+    queryset = APODImage.objects.all()
+    serializer_class = APODImageSerializer
+    lookup_field = "date"
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
