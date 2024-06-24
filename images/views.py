12d31d3015fada1253c7b4dee7542c126d86009a
Leah Rivkin <30099399+lrivkin@users.noreply.github.com>
2024-06-23 19:10:14 -0600

    works with image manually inserted to database

--- images/views.py
+++ images/views.py
@@ -5,14 +5,8 @@ from rest_framework.response import Response
 from .models import APODImage
 from .serializers import APODImageSerializer
 
+# LOG = logger.getLogger(__name__)
 class APODImageAPIView(generics.RetrieveAPIView):
     queryset = APODImage.objects.all()
     serializer_class = APODImageSerializer
     lookup_field = "date"
-
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-
-        serializer = self.get_serializer(instance)
-        data = serializer.data
-        return Response(data)
