04410f3649da060fab1a85d31b3e430575113764
Leah Rivkin <30099399+lrivkin@users.noreply.github.com>
2024-06-25 22:05:47 -0600

    make hdurl optional

--- images/models.py
+++ images/models.py
@@ -5,7 +5,7 @@ class APODImage(models.Model):
     copyright = models.CharField(max_length=255, blank=True, null=True)
     date = models.DateField()
     explanation = models.TextField()
-    hdurl = models.URLField()
+    hdurl = models.URLField(blank=True, null=True)
     media_type = models.CharField(max_length=50)
     service_version = models.CharField(max_length=10)
     title = models.CharField(max_length=255)
