e286f6730a5492959f5d49b3ea6869dd1f1dc861
Leah Rivkin <30099399+lrivkin@users.noreply.github.com>
2024-06-27 08:21:12 -0600

    Add tags to model

--- images/models.py
+++ images/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class APODImage(models.Model):
     copyright = models.CharField(max_length=255, blank=True, null=True)
     date = models.DateField()
@@ -12,6 +19,7 @@ class APODImage(models.Model):
     url = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tag, related_name="images", blank=True)
 
     def __str__(self):
         return f"{self.title} ({self.date})"
